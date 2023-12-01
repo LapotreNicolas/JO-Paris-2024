3925f53edfb4938b11a0f57846b30db78c090918
nicolas_lapotre <nicolas_lapotre@ens.univ-artois.fr>
2023-12-01 17:29:48 +0100

    Ajout de méthode dans le Controller

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -4,19 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Models\Sport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class SportController extends Controller
 {
-    public function index(Request $request)
-    {
-        $taches = Sport::all();
-        $nom = $request->get("nom", '');
-        $sports = [];
-        if (empty($nom)) {
-            $taches = Sport::all();
+    public function index(Request $request) {
+        $nb = $request->input('nb', null);
+        $cookieNb = $request->cookie('nb', null);
+
+        if (!isset($nb)) {
+            if (!isset($cookieNb)) {
+                $sports = Sport::all();
+                $nb = 'All';
+                Cookie::expire('nb');
+            } else {
+                $sports = Sport::where('nb_epreuves', $cookieNb)->get();
+                $nb = $cookieNb;
+                Cookie::queue('nb', $nb, 10);            }
         } else {
-            $taches = Sport::where('nom', '=', $nom)->get();
+            if ($nb == 'All') {
+                $sports = Sport::all();
+                Cookie::expire('nb');
+            } else {
+                $sports = Sport::where('nb_epreuves', $nb)->get();
+                Cookie::queue('nb', $nb, 10);
+            }
         }
-        return view('sports.index', ['sports' => $sports]);
+        $nb_epreuves = Sport::distinct('nb_epreuves')->pluck('nb_epreuves');
+        return view('sports.index', ['sports' => $sports, 'nb' => $nb, 'nb_epreuves' => $nb_epreuves, 'cookiesNb' => $cookieNb]);
+    }
+
+    public function upload(Request $request, $id) {
+        $sport = Sport::findOrFail($id);
+        if ($request->hasFile('document') && $request->file('document')->isValid()) {
+            $file = $request->file('document');
+        } else {
+            $msg = "Aucun fichier téléchargé";
+            return redirect()->route('sports.show', [$sport->id])
+                ->with('type', 'primary')
+                ->with('msg', 'Smartphone non modifié ('. $msg . ')');
+        }
+        $nom = 'image';
+        $now = time();
+        $nom = sprintf("%s_%d.%s", $nom, $now, $file->extension());
+
+        $file->storeAs('images', $nom);
+        if (isset($sport->url_media)) {
+            Log::info("Image supprimée : ". $sport->url_media);
+            Storage::delete($sport->url_media);
+        }
+        $sport->url_media = 'images/'.$nom;
+        $sport->save();
+        //$file->store('docs');
+        return redirect()->route('sports.show', [$sport->id])
+            ->with('type', 'primary')
+            ->with('msg', 'Tâche modifiée avec succès');
     }
 }
