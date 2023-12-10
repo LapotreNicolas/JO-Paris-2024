938d41bf394c8106b6b9737d5d8916051f79d53f
Nicolas <nicolas_lapotre@ens.univ-artois.fr>
2023-12-10 10:07:05 +0100

    Ajout des composants et du premier css

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ route('accueil') }}"><button>🏛 Accueil</button></a>
+<a href="{{ route('sports.index') }}"><button>🥇 Sports</button></a>
+<a href="{{ route('apropos') }}"><button>ℹ️ A propos</button></a>
+<a href="{{ route('contact') }}"><button> Contact</button></a>
